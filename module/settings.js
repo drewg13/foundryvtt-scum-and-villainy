744e04b0859efe89c01f2ff8806db4bc9f17ebeb
drewg13 <drewg13@users.noreply.github.com>
2021-01-23 16:39:43 -0500

    Update settings.js

--- module/settings.js
+++ module/settings.js
@@ -3,7 +3,7 @@ export const registerSystemSettings = function() {
   /**
    * Track the system version upon which point a migration was last applied
    */
-  game.settings.register("bitd", "systemMigrationVersion", {
+  game.settings.register("sav", "systemMigrationVersion", {
     name: "System Migration Version",
     scope: "world",
     config: false,
