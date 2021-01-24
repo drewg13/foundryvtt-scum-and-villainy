4fae20d07c62ccb991a8309c5d9715480468cd07
drewg13 <drewg13@users.noreply.github.com>
2021-01-23 21:28:31 -0500

    Update blades-item.js

--- module/blades-item.js
+++ module/blades-item.js
@@ -30,7 +30,7 @@ export class BladesItem extends Item {
     
     // calculates upkeep value from (crew quality + engine quality + hull quality + comms quality + weapons quality) / 4, rounded down
     if (this.actor) {
-        upkeep = Math.floor((parseInt(this.actor.data.data.crew) + parseInt(this.data.data.systems.engines.value) + parseInt(this.data.data.systems.hull.value) + parseInt(this.data.data.systems.comms.value) + parseInt(this.data.data.systems.weapons.value)) / 4);
+        upkeep = Math.floor((parseInt(data.crew) + parseInt(data.systems.engines.value) + parseInt(data.systems.hull.value) + parseInt(data.systems.comms.value) + parseInt(data.systems.weapons.value)) / 4);
         }
 
     this.actor.data.data.upkeep = upkeep;
