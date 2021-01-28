f8c1b1baa1acc651577927b300dda8dc81d4b63b
drewg13 <drewg13@users.noreply.github.com>
2021-01-28 11:02:33 -0500

    Update blades-item-sheet.js

--- module/blades-item-sheet.js
+++ module/blades-item-sheet.js
@@ -20,7 +20,7 @@ export class BladesItemSheet extends ItemSheet {
   /** @override */
   get template() {
     const path = "systems/scum-and-villainy/templates/items";
-    let simple_item_types = ["background", "heritage", "vice", "crew_reputation"];
+    let simple_item_types = ["background", "heritage", "vice", "crew_reputation", "ship_size"];
     let template_name = `${this.item.data.type}`;
 
     if (simple_item_types.indexOf(this.item.data.type) >= 0) {
