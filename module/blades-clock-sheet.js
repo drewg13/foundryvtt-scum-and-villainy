e97c1b234477e12ba4e22fad837c68448582b847
drewg13 <drewg13@users.noreply.github.com>
2021-02-14 00:03:03 -0500

    Sci-fi and 10/12 clocks

--- module/blades-clock-sheet.js
+++ module/blades-clock-sheet.js
@@ -12,7 +12,7 @@ export class BladesClockSheet extends BladesSheet {
 	  return mergeObject(super.defaultOptions, {
   	  classes: ["scum-and-villainy", "sheet", "actor"],
   	  template: "systems/scum-and-villainy/templates/actors/clock-sheet.html",
-      width: 215,
+      width: 270,
       height: 350,
     });
   }
@@ -22,7 +22,7 @@ export class BladesClockSheet extends BladesSheet {
   /** @override */
   async _updateObject(event, formData) {
 
-    let image_path = `/systems/scum-and-villainy/styles/assets/progressclocks-svg/Progress Clock ${formData['data.type']}-${formData['data.value']}.svg`;
+    let image_path = `/systems/scum-and-villainy/styles/assets/progressclocks/Progress Clock ${formData['data.type']}-${formData['data.value']}.webp`;
     formData['img'] = image_path;
     formData['token.img'] = image_path;
 
