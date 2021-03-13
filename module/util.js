1544638be4237a7ac84c2fcf7906273c395e9ee2
drewg13 <drewg13@users.noreply.github.com>
2021-03-13 15:06:04 -0500

    Delete util.js

--- module/util.js
+++
@@ -1,3 +0,0 @@
-export const log = (message) => console.log(`Foundry VTT | Clocks | ${message}`);
-export const warn = (message) => console.warn(`Foundry VTT | Clocks | ${message}`);
-export const error = (message) => console.error(`Foundry VTT | Clocks | ${message}`);
